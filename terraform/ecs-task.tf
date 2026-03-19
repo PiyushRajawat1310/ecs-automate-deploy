be61c9124db3897815db396a134d4918beac53ae
PiyushRajawat1310 <223863796+PiyushRajawat1310@users.noreply.github.com>
2026-03-20 03:20:38 +0530

    Knight Rider

--- terraform/ecs-task.tf
+++ terraform/ecs-task.tf
@@ -20,6 +20,21 @@ resource "aws_ecs_task_definition" "strapi" {
         }
       ]
 
+      environment = [
+        { name = "HOST", value = "0.0.0.0" },
+        { name = "PORT", value = "1337" },
+
+        # 🔐 APP_KEYS (first 4 keys, comma-separated)
+        { name = "APP_KEYS", value = "1WkQyYeN0VQHk/ISVSTAB8eS2J2N8NJU5v1KedpAhds=,6OJJ9QvwFXhFk77nqlN6mCFLJ6s33xCZUG7sGk5oJAs=,dasahCUFTPP3yfr5ZgpoL7ZNn+6TbNhjWKaFoTxzgGQ=,BGCDfjSB/ttNRdKaN5PomlkfFuE7zXfvLt9RQa3Ba+s=" },
+
+        # 🔐 Remaining secrets
+        { name = "API_TOKEN_SALT", value = "Dob25C5G2bMVWN8fgVasDFCNbwfXfT+9esj6ZyF40Es=" },
+        { name = "ADMIN_JWT_SECRET", value = "/gkBZxZ5Prg6Os4D5vKcUDC0nPXBKUtywW8tTBBaMhw=" },
+        { name = "TRANSFER_TOKEN_SALT", value = "frpt4ZJ/yzv1moBqmynkm45xX65QHE3D/HeSXKJdnts=" },
+        { name = "JWT_SECRET", value = "Lc8rc+l54sCqv+YtQkA2fPDd25HFiBwjkq2YD5WUh4o=" },
+        { name = "ENCRYPTION_KEY", value = "wKKEDBiqRQ7goy9MmZBAlVveIU316Tu991Ph9ALyVH0=" }
+      ]
+
       logConfiguration = {
         logDriver = "awslogs"
         options = {
@@ -31,4 +46,3 @@ resource "aws_ecs_task_definition" "strapi" {
     }
   ])
 }
-
