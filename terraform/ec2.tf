309e58b759fd87dcd28cc9fd067592d29312693a
piyushrajawat1310 <piyushrajawat1310@gmail.com>
2026-03-21 08:00:44 +0000

    adding ec2 service

---
+++ terraform/ec2.tf
@@ -0,0 +1,70 @@
+data "aws_ami" "ecs_optimized" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-ecs-hvm-*-x86_64-ebs"]
+  }
+
+  owners = ["amazon"]
+}
+
+resource "aws_iam_role" "ecs_instance_role" {
+  name = "ecsInstanceRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_role_attach" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "ecsInstanceProfile"
+  role = aws_iam_role.ecs_instance_role.name
+}
+
+resource "aws_security_group" "ecs_instance_sg" {
+  name = "ecs-instance-sg"
+
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "ecs_instance" {
+  ami           = data.aws_ami.ecs_optimized.id
+  instance_type = "t3.micro"
+
+  iam_instance_profile = aws_iam_instance_profile.ecs_instance_profile.name
+  security_groups      = [aws_security_group.ecs_instance_sg.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo ECS_CLUSTER=strapi-cluster >> /etc/ecs/ecs.config
+              EOF
+
+  tags = {
+    Name = "ecs-strapi-instance"
+  }
+}
